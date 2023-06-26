bd86ef20e83694b05d32a530fc0f7b6571bd0dab
wjkk21 <verstand821@gmail.com>
2023-06-26 16:23:15 +0900

    nest.js 재 설치

--- client/App.tsx
+++ client/App.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import Test from "./index" 
+import React from 'react';
+import Test from './index';
 
 const component: React.FC = () => {
   return (
     <>
-		  <h1>First Page</h1>
-		  <Test name={이 문구가 보이면 성공입니다.} />
+      <h1>First Page</h1>
+      <Test name=" 문구가 보이면 성공입니다." />
     </>
-	)
-}
+  );
+};
 
 export default component;
